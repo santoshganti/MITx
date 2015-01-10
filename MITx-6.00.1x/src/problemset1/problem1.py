4c9b45a0b5137b2e3ee1fb0e622e26289fd08663
santoshganti <santosh.ganti@me.com>
2015-01-10 21:35:38 +0530

    new modules

--- MITx-6.00.1x/src/problemset1/problem1.py
+++ MITx-6.00.1x/src/problemset1/problem1.py
@@ -14,9 +14,9 @@ print Goodbye!
 '''
 limit = 10
 x = 2
-while x<=limit:
-    if x%2==0:
+while x <= limit:
+    if x % 2 == 0:
         print x
-        x+=2
+        x += 2
 print "Goodbye!"    
         

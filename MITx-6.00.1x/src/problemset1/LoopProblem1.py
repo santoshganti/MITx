f7e79534a0cb7881100eb8943bbb3ce63b7b541b
santoshganti <santosh.ganti@me.com>
2015-01-10 23:19:59 +0530

    New Modules

---
+++ MITx-6.00.1x/src/problemset1/LoopProblem1.py
@@ -0,0 +1,18 @@
+'''
+Created on 10-Jan-2015
+
+@author: santoshganti
+
+1. Convert the following code into code that uses a for loop.
+
+print 2
+print 4
+print 6
+print 8
+print 10
+print "Goodbye!"
+
+'''
+for number in range(2,11,2):
+    print number
+print 'Goodbye!'

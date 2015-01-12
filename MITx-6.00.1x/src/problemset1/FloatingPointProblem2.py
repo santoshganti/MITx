ceb05a8b091e40e8c376da317ed47f4bc911e504
santoshganti <santosh.ganti@me.com>
2015-01-12 19:12:06 +0530

    New Files

---
+++ MITx-6.00.1x/src/problemset1/FloatingPointProblem2.py
@@ -0,0 +1,20 @@
+'''
+Created on 12-Jan-2015
+
+@author: santoshganti
+'''
+x = 23
+epsilon = 0.01
+step = 0.1
+guess = 0.0
+
+while abs(guess**2-x) >= epsilon:
+    if guess <= x:
+        guess += step
+    else:
+        break
+
+if abs(guess**2 - x) >= epsilon:
+    print 'failed'
+else:
+    print 'succeeded: ' + str(guess)

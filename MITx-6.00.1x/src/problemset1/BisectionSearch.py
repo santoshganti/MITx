06355ca4f3d609f7d94f6de099404bd1324514aa
santoshganti <santosh.ganti@me.com>
2015-01-12 19:49:55 +0530

    New Classes

---
+++ MITx-6.00.1x/src/problemset1/BisectionSearch.py
@@ -0,0 +1,21 @@
+'''
+Created on 12-Jan-2015
+
+@author: santoshganti
+'''
+x = 9
+epsilon = 0.01
+numGuesses = 0
+low = 0.0
+high = x
+ans = (high + low)/2.0
+while abs(ans**2 - x) >= epsilon:
+    print('low = ' + str(low) + ' high = ' + str(high) + ' ans = ' + str(ans))
+    numGuesses += 1
+    if ans**2 < x:
+        low = ans
+    else:
+        high = ans
+    ans = (high + low)/2.0
+print('numGuesses = ' + str(numGuesses))
+print(str(ans) + ' is close to square root of ' + str(x))
--- MITx-6.00.1x/src/problemset1/DecimalToBinary.py
+++ MITx-6.00.1x/src/problemset1/DecimalToBinary.py
@@ -5,18 +5,18 @@ Created on 11-Jan-2015
 
 Program to convert decimal into binary
 '''
-num = 256
+num = -256
 if num < 0:
     isNeg = True
     num = abs(num)
 else:
     isNeg = False
-    result = ''
+result = ''
 if num == 0:
     result = '0'
-while num > 2:
-        result = str(num % 2) + result
-        num = num / 2
+while num > 0:
+        result = str(num % 2) + result 
+        num = num / 2 
 if isNeg:
         result = '-' + result
 print result

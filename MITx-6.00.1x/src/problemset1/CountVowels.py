f9eac9d2fe264d2e5e440c9876750f11a8639934
santoshganti <santosh.ganti@me.com>
2015-01-14 16:31:03 +0530

    New files

---
+++ MITx-6.00.1x/src/problemset1/CountVowels.py
@@ -0,0 +1,16 @@
+'''
+Created on 14-Jan-2015
+
+@author: santoshganti
+
+Assume s is a string of lower case characters.
+
+Write a program that counts up the number of vowels contained in the string s. 
+Valid vowels are: 'a', 'e', 'i', 'o', and 'u'. 
+For example, if s = 'azcbobobegghakl', your program should print:
+
+Number of vowels: 5
+'''
+s = 'arwrulenpoauyacmm'
+countA = s.lower().count('a') + s.lower().count('e')+s.lower().count('i')+s.lower().count('o')+s.lower().count('u')
+print "Number of vowels: " +str(countA)
---
+++ MITx-6.00.1x/src/problemset1/Odd.py
@@ -0,0 +1,10 @@
+'''
+Created on 14-Jan-2015
+
+@author: santoshganti
+'''
+def Odd(x):
+    return bool(x%2!=0)
+
+print Odd(3)
+print Odd(2)

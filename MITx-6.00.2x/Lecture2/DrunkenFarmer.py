fe47fc2bf01dc125926c1d44ef8964f6b08132d5
santoshganti <santosh.ganti@me.com>
2015-03-17 18:31:58 +0530

    Added more functionality

--- MITx-6.00.2x/Lecture2/DrunkenFarmer.py
+++ MITx-6.00.2x/Lecture2/DrunkenFarmer.py
@@ -32,3 +32,44 @@ class Location(object):
     # A way to print the location
     def __str__(self):
         return '<'+self.x+', '+self.y+'>'
+
+
+class Field(object):
+    #init
+    def __init__(self):
+        self.drunks = {}
+    #add drunk method
+    def addDrunk(self, drunk, loc):
+        if drunk in self.drunks:
+            raise ValueError('Duplicate Drunk')
+        else:
+            self.drunks[drunk] = loc
+    #move drunk method
+    def moveDrunk(self, drunk):
+        if not drunk in self.drunks:
+            raise ValueError('Not in field')
+        xDist, yDist = drunk.takeStep()
+        currentLocation = self.drunks[drunk]
+        self.drunks[drunk] = currentLocation.move(xDist, yDist)
+
+    #get location of the drunk
+    def getLoc(self, drunk):
+        if drunk not in self.drunks:
+            raise ValueError('Drunk not in field')
+        return self.drunks[drunk]
+
+import random
+
+
+class Drunk(object):
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return "This drunk is named "+self.name
+
+
+class UsualDrunk(Drunk):
+    def takeStep(self):
+        stepChoices =  [(0.0,1.0), (0.0,-1.0), (1.0, 0.0), (-1.0, 0.0)]
+        return random.choice(stepChoices)
